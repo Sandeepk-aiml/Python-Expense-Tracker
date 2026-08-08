419dddce949ae65f1398c0732f163e82c3cd3ac3
Sandeep <kumarsandeep40702@gmail.com>
2026-08-08 23:02:18 +0530

    Create basic expense tracker

---
+++ main.py
@@ -0,0 +1,9 @@
+item = input("Enter your item name : ")
+amount =float(input("Enter price of item : "))
+
+print("=====Expense Added=====")
+
+print(f"Item : {item}")
+print(f"Amount : {amount}")
+
+print(f"you spent {amount} rupees on {item}")
