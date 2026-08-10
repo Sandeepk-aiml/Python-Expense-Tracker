8959a25cc6bf7004078d7b4608573e0656da79ff
Sandeep <kumarsandeep40702@gmail.com>
2026-08-10 19:10:59 +0530

    Improve expense display and input validation

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
 number = int(input("How many expenses do you want to enter? "))
 
+while number <= 0:
+    print("Please enter a number greater than 0.")
+    number= int(input("Enter number of expenses : "))
+
+
 expenses = []
 
 for i in range(number):
@@ -11,12 +16,32 @@ for i in range(number):
  
 print("=====Expense Added=====")
 
-for expense in expenses:
-    print(expense[0], expense[1])
 
-total = 0
+for i, expense in enumerate(expenses, start=1):
+    print(f"{i}. {expense[0]} rs{expense[1]:.2f}")
+    
 
+total  = 0
+ 
 for expense in expenses:
     total = total + expense[1]
 
-print(total)
+print(f"\nTotal spent: Rs{total:.2f}")
+
+
+
+# number = int(input("How many expenses do you want to enter? "))
+
+# while number <= 0:
+#     print("Please enter a number greater than 0.")
+#     number= int(input("Enter number of expenses : "))
+
+
+
+
+
+
+
+
+
+
